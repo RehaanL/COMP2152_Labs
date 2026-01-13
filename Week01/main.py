726bc8ae672d194f73deb71550dd8d494bb30e28
rehaanlachporia <rehaanlachporia@gmail.com>
2026-01-13 13:51:03 -0500

    add solutions for Week01

---
+++ Week01/main.py
@@ -0,0 +1,27 @@
+# Sample Coding Questions Week 1
+# Rehaan Lachporia 101594859
+
+## Defining Variables
+my_array = [1, 4, 7, 9]
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+
+## Order of Operations
+e = a - ((b ** c) // d) + (a % c)
+print(e)
+
+## Formatting
+temperature = 32.6
+sentence = f"The temperature is {temperature:.3f} degrees celsius"
+print(sentence)
+
+## Common Functions
+user_age = input("Please enter your age: ")
+user_age = int(user_age) + 22
+
+sentence = f"Now showing shop items filtered by age: {user_age}"
+print(sentence)
